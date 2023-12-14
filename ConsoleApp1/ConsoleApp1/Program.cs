1bfa2c59052bac1c057ce5cbd3f135836b93755f
DarshakAcharya <darshakacharya2625@gmail.com>
2023-12-14 09:39:36 +0530

    Added line in Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I have done Renaming operation of branches and currently trying to Push the changes!");
 Console.WriteLine("I have added this line from GitHub Edit!");
+Console.WriteLine("Trying the ammend command. First Commit then Ammend!");
