ebb562ef098609fdb4ef003c71e415e46de01c39
DarshakAcharya <darshakacharya2625@gmail.com>
2023-12-14 10:48:29 +0530

    Added features: 2,3,4,5

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,3 +5,5 @@ Console.WriteLine("I have added this line from GitHub Edit!");
 Console.WriteLine("Trying the ammend command. First Commit then Ammend!");
 
 Console.WriteLine("Added feature: feature1");
+
+Console.WriteLine("Added features: feature2, feature3, feature4, feature5");
